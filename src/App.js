a789bc6c03c270eb36fc3cbec7d1b97209431010
Ankit kumar Gupta <119320392+ANKIT632@users.noreply.github.com>
2023-11-05 19:11:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React,{useState}from 'react'
 function App() {
   const [data,setData]=useState("Anil")
   return (
-    <div className="App">
+    <div className="App"> 
       <h1>Hooks in React {data}</h1>
       <button onClick={()=>setData("Peter")}>Update State</button>
     </div>
